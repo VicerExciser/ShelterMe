4e9aafdfaed7c2eab3df67a35b24e0665359585f
kleroux3@gatech.edu <bobert97>
2018-02-12 21:01:56 -0500

    button text

--- app/src/main/java/edu/gatech/cs2340/shelterme/HomePage.java
+++ app/src/main/java/edu/gatech/cs2340/shelterme/HomePage.java
@@ -3,7 +3,7 @@ package edu.gatech.cs2340.shelterme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -11,7 +11,7 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        ImageButton logout = findViewById(R.id.logoutbutton);
+        Button logout = findViewById(R.id.logoutbutton);
         logout.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 android.content.Intent myIntent1 = new android.content.Intent(view.getContext(), MainActivity.class);
