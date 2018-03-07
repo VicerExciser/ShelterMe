7530bcb03d63c7709fd9da4f0f0380aaeab1162e
kleroux3@gatech.edu <bobert97>
2018-03-07 01:04:32 -0500

    search options (front end)

--- app/src/main/java/edu/gatech/cs2340/shelterme/controllers/ViewSheltersPage.java
+++ app/src/main/java/edu/gatech/cs2340/shelterme/controllers/ViewSheltersPage.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -17,6 +21,8 @@ import java.util.HashMap;
 import java.util.List;
 
 import edu.gatech.cs2340.shelterme.R;
+import edu.gatech.cs2340.shelterme.model.Account;
+import edu.gatech.cs2340.shelterme.model.Age;
 import edu.gatech.cs2340.shelterme.model.Model;
 import edu.gatech.cs2340.shelterme.model.Shelter;
 import edu.gatech.cs2340.shelterme.model.User;
@@ -26,6 +32,7 @@ public class ViewSheltersPage extends AppCompatActivity {
     private ArrayAdapter<String> adapter;
     private HashMap<String, Shelter> shelters;
     private Model model = Model.getInstance();
+    private boolean critChecked;
 
 
     @Override
@@ -41,18 +48,66 @@ public class ViewSheltersPage extends AppCompatActivity {
             }
         });
 
+        //default to registered criteria
+        final Button criteriaCheck = (Button) findViewById(R.id.yesCrit);
+        final Button noCrit = (Button) findViewById(R.id.noCrit);
+        final Spinner ageSpin = (Spinner) findViewById(R.id.ageSpinner);
+        final Spinner genderSpin = (Spinner) findViewById(R.id.genderSpinner);
+        final CheckBox familyCheck = (CheckBox) findViewById(R.id.FamilyCheck);
+        final TextView text = (TextView) findViewById(R.id.inferCriteria);
+        ageSpin.setVisibility(View.GONE);
+        genderSpin.setVisibility(View.GONE);
+        familyCheck.setVisibility(View.GONE);
+
+
+
+
+        //filling spinners
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, Age.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ageSpin.setAdapter(adapter);
+
+        ArrayAdapter<String> adapterGen = new ArrayAdapter(this,android.R.layout.simple_spinner_item, Account.Sex.values());
+        adapterGen.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpin.setAdapter(adapterGen);
+
         shelters = new HashMap<>();
         for (Shelter s : Model.getShelterListPointer()) {
             shelters.put(s.getShelterName(), s);
         }
-        List<String> list = new ArrayList<>(shelters.keySet());
-        for (int i = 0; i < shelters.size(); i++) {
-            if (((User) model.getCurrUser()) != null) {
-                if (!shelters.get(list.get(i)).hasOpenBed(((User) model.getCurrUser()).generateKey())) {
-                    shelters.remove(shelters.get(list.get(i)));
+
+        noCrit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                ageSpin.setVisibility(View.VISIBLE);
+                genderSpin.setVisibility(View.VISIBLE);
+                familyCheck.setVisibility(View.VISIBLE);
+                criteriaCheck.setVisibility(View.GONE);
+                noCrit.setVisibility(View.GONE);
+                text.setVisibility(View.GONE);
+                critChecked = false;
+            }
+        });
+
+        criteriaCheck.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                critChecked = true;
+                List<String> list = new ArrayList<>(shelters.keySet());
+                for (int i = 0; i < shelters.size(); i++) {
+                    if (((User) model.getCurrUser()) != null) {
+                        if (!shelters.get(list.get(i)).hasOpenBed(((User) model.getCurrUser()).generateKey())) {
+                            shelters.remove(shelters.get(list.get(i)));
+                        }
+                    }
                 }
             }
-        }
+        });
+
+
+
+
+
         ListView shelterList = findViewById(R.id.shelterList);
 
         SearchView inputSearch = findViewById(R.id.inputSearch);
@@ -102,6 +157,7 @@ public class ViewSheltersPage extends AppCompatActivity {
 //            }
 //        });
     }
+
 
     private void displayDetailView(int position) {
         Shelter selected = shelters.get(adapter.getItem(position));
