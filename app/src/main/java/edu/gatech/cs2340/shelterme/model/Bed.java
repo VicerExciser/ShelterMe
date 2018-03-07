fee0f73c745b219586dce77d35e1b8fe839e52fe
Russell Joseph Conklin <rconklin6@gatech.edu>
2018-03-06 21:30:03 -0500

    Added requisite functionality to the Bed class for bed-building.

--- app/src/main/java/edu/gatech/cs2340/shelterme/model/Bed.java
+++ app/src/main/java/edu/gatech/cs2340/shelterme/model/Bed.java
@@ -6,12 +6,64 @@ package edu.gatech.cs2340.shelterme.model;
 
 public class Bed {
 
-    User occupant;
-    boolean isOccupied;
-    boolean isFamily;       //designates if the bed is of "family type" or for a single person
-    boolean noAdultMen;     //designates if the bed is a "women and children only" bed
-    int minAge;             //designates minimum age that applies to an individual or the children of a family
-    int maxAge;             //designates maximum age that applies to an individual or the children of a family
-    boolean veteranOnly;    //designates if the bed must have at least one veteran occupying it
+    private int id;
+    private User occupant;
+    private boolean isOccupied;
+    private boolean isFamily;       //designates if the bed is of "family type" or for a single person
+    private boolean noAdultMen;     //designates if the bed is a "women and children only" bed
+    private Age minAge;             //designates minimum age that applies to an individual or the children of a family
+    private Age maxAge;             //designates maximum age that applies to an individual or the children of a family
+    private boolean veteranOnly;    //designates if the bed must have at least one veteran occupying it
 
+    public Bed(int id, boolean isFamily, boolean noAdultMen, Age minAge, Age maxAge, boolean veteranOnly) {
+        this.id = id;
+        this.isOccupied = false;
+        this.isFamily = isFamily;
+        this.noAdultMen = noAdultMen;
+        this.minAge = minAge;
+        this.maxAge = maxAge;
+        this.veteranOnly = veteranOnly;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User getOccupant() {
+        return occupant;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isFamily() {
+        return isFamily;
+    }
+
+    public boolean isNoAdultMen() {
+        return noAdultMen;
+    }
+
+    public Age getMinAge() {
+        return minAge;
+    }
+
+    public Age getMaxAge() {
+        return maxAge;
+    }
+
+    public boolean isVeteranOnly() {
+        return veteranOnly;
+    }
+
+    public void setOccupant(User occupant) {
+        this.occupant = occupant;
+        this.isOccupied = true;
+    }
+
+    public void removeOccupant() {
+        this.occupant = null;
+        this.isOccupied = false;
+    }
 }
