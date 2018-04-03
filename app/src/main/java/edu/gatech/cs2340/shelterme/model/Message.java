a2d2c53ba82e1867239090cb24a799db20cf4f15
Russell Joseph Conklin <rconklin6@gatech.edu>
2018-04-03 19:19:44 -0400

    Implemented all kinds of Messages and MessageBoardModel.

--- app/src/main/java/edu/gatech/cs2340/shelterme/model/Message.java
+++ app/src/main/java/edu/gatech/cs2340/shelterme/model/Message.java
@@ -1,8 +1,62 @@
 package edu.gatech.cs2340.shelterme.model;
 
+import java.util.Calendar;
+
 /**
  * Created by austincondict on 2/21/18.
  */
 
 public class Message {
+    private String message;
+    private String timeSent;
+    private String senderEmail;
+    private boolean isAddressed;
+
+    public Message(String message, Account sender) {
+        this.message = message;
+        this.timeSent = Calendar.getInstance().getTime().toString();
+        this.senderEmail = sender.getEmail();
+        this.isAddressed = false;
+    }
+
+    public Message() {
+        this("No Message Given", new User());
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTimeSent() {
+        return timeSent;
+    }
+
+    public void setSenderEmail(String senderEmail) {
+        this.senderEmail = senderEmail;
+    }
+
+    public String getSenderEmail() {
+        return senderEmail;
+    }
+
+    public void setTimeSent(String timeSent) {
+        this.timeSent = timeSent;
+    }
+
+    public boolean isAddressed() {
+        return isAddressed;
+    }
+
+    public void setAddressed(boolean addressed) {
+        isAddressed = addressed;
+    }
+
+    public void address() {
+        isAddressed = true;
+    }
 }
---
+++ app/src/main/java/edu/gatech/cs2340/shelterme/model/MessageBoardModel.java
@@ -0,0 +1,34 @@
+package edu.gatech.cs2340.shelterme.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MessageBoardModel {
+    public static List<Message> messages = new LinkedList<>();
+    public static List<Message> unaddressedMessages = new LinkedList<>();
+
+    public static void addressMessage(Message message) {
+        message.address();
+        unaddressedMessages.remove(message);
+    }
+
+    private static void addMessageToMessageBoard(Message message) {
+        messages.add(message);
+        unaddressedMessages.add(message);
+    }
+
+    public static void sendNewMessage(String message, User sender) {
+        Message newMessage = new Message(message, sender);
+        addMessageToMessageBoard(newMessage);
+    }
+
+    public static void sendNewReportUserRequest(User reportedUser, String message, Employee sender) {
+        ReportUserRequest reportUserRequest = new ReportUserRequest(reportedUser, message, sender);
+        addMessageToMessageBoard(reportUserRequest);
+    }
+
+    public static void sendNewUnlockRequest(User lockedUser, String message, Account sender) {
+        UnlockRequest unlockRequest = new UnlockRequest(lockedUser, message, sender);
+        addMessageToMessageBoard(unlockRequest);
+    }
+}
